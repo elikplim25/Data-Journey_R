05c0e1d09ec8e2f22fdeb595be3a84a9e1e62ab5
Eli Sabblah <elisabblah@gmail.com>
2023-08-03 21:03:49 +0000

    Testing R and Git Hub connection.

--- linear regression.R
+++ linear regression.R
@@ -1,39 +1,73 @@
+##############################################################################
 
-head(cars)
 
+library(tidyverse)
+library(Lahman)
+head(Teams)
 
-## regression analysis visualized in a scatter plot
-ggplot(cars, aes(speed, dist))+
+### runs per game vs home runs ######
+
+Teams %>%
+  filter(yearID %in% 1961:2001)%>%
+  ggplot(aes(R, HR))+
+  geom_point(alpha = 0.5)+
+  geom_smooth(method = "lm")+
+  theme_bw()
+
+#### runs per game vs at bats #####
+
+Teams %>%
+  filter(yearID %in% 1961:2001)%>%
+  ggplot(aes(R, AB))+
+  geom_point(alpha = 0.5)+
+  #geom_smooth(method = "lm")+
+  theme_bw()
+
+###win rate (number of wins per game) versus number of fielding errors (E)###
+
+Teams %>%
+  filter(yearID %in% 1961:2001)%>%
+  ggplot(aes(X3B, X2B))+
+  geom_point(alpha = 0.5)+
+  theme_bw()+
+  geom_smooth(method = "lm")
+
+### correlation #####
+
+wins <- Teams_filtered$W
+errors <- Teams_filtered$E
+cor(wins, errors)
+
+doubles <- Teams_filtered$X2B
+triples <- Teams_filtered$X3B
+cor(doubles, triples)
+
+##############################################################################
+
+#### Galton's inception of regression and correlation ###############
+
+library(HistData)
+
+head(GaltonFamilies)
+dim(GaltonFamilies)
+
+##### extracting data for analysis ########
+
+galton_heights <- GaltonFamilies %>%
+  filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+view(galton_heights)
+
+###### visualizing data #########
+
+galton_heights%>%  
+ggplot(aes(father, son))+
   geom_point()+
-  geom_smooth(method = lm)
-
-## Box plot visualization
-ggplot(cars, aes(speed, dist))+ 
-  geom_boxplot()
-
-## Density plots for speed and distance
-ggplot(cars, aes(dist))+ 
-  geom_density(fill = "red4")
-
-ggplot(cars, aes(speed))+ 
-  geom_density(fill = "steelblue")
-
-## Correlation
-cor(cars$speed, cars$dist)
-
-linearmod <- lm(dist ~ speed, cars)
-
-summary(linearmod)
-coefficients(linearmod)
-coef(linearmod)
-errorCondition(linearmod)
+  geom_smooth(method = "lm")
 
 
 
-
-
-
-
-
-
-theme(strip.text = element_textbox)
